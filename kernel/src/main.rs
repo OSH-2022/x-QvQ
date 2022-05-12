35aab0b4720c89a6b4d1810ceffcc0531e672fc0
jiuguaiwf <jiuguaiwf_@mail.ustc.edu.cn>
2022-05-13 00:03:47 +0800

    add trap feature

--- kernel/src/main.rs
+++ kernel/src/main.rs
@@ -6,10 +6,13 @@ mod arch;
 mod bsp;
 mod panic;
 mod print;
+mod trap;
 
 #[no_mangle]
 fn _start_rust() {
     bsp::driver_init();
     print!("== Kernel Init ==\n").unwrap();
+    trap::init();
+    print!("==trap available==\n").unwrap();
     loop {}
 }
---
+++ kernel/src/trap/mod.rs
@@ -0,0 +1,107 @@
+mod context;
+pub use context::TrapFrame;
+
+use core::arch::global_asm;
+
+use cortex_a::registers::{ESR_EL1, FAR_EL1, VBAR_EL1};
+use tock_registers::interfaces::{Readable, Writeable};
+
+// use crate::{syscall::syscall, task::CurrentTask};
+
+global_asm!(include_str!("trap.S"));
+
+pub fn init() {
+    extern "C" {
+        fn exception_vector_base();
+    }
+    VBAR_EL1.set(exception_vector_base as usize as _);
+}
+
+#[repr(u8)]
+#[derive(Debug)]
+#[allow(dead_code)]
+enum TrapKind {
+    Synchronous = 0,
+    Irq = 1,
+    Fiq = 2,
+    SError = 3,
+}
+
+#[repr(u8)]
+#[derive(Debug)]
+#[allow(dead_code)]
+enum TrapSource {
+    CurrentSpEl0 = 0,
+    CurrentSpElx = 1,
+    LowerAArch64 = 2,
+    LowerAArch32 = 3,
+}
+
+#[derive(Debug, Eq, PartialEq)]
+pub enum IrqHandlerResult {
+    Reschedule,
+    NoReschedule,
+}
+
+#[no_mangle]
+fn invalid_exception(tf: &mut TrapFrame, kind: TrapKind, source: TrapSource) {
+    panic!(
+        "Invalid exception {:?} from {:?}:\n{:#x?}",
+        kind, source, tf
+    );
+}
+
+#[no_mangle]
+fn handle_sync_exception(tf: &mut TrapFrame) {
+    // let esr = ESR_EL1.extract();
+    // match esr.read_as_enum(ESR_EL1::EC) {
+    //     Some(ESR_EL1::EC::Value::Unknown) => {
+    //         println!(
+    //             "[kernel] Unknown exception @ {:#x}, kernel killed it.",
+    //             tf.elr
+    //         );
+    //         CurrentTask::get().exit(-1);
+    //     }
+    //     Some(ESR_EL1::EC::Value::SVC64) => {
+    //         tf.r[0] = syscall(tf.r[8] as _, [tf.r[0] as _, tf.r[1] as _, tf.r[2] as _], tf) as u64
+    //     }
+    //     Some(ESR_EL1::EC::Value::DataAbortLowerEL)
+    //     | Some(ESR_EL1::EC::Value::DataAbortCurrentEL) => {
+    //         let iss = esr.read(ESR_EL1::ISS);
+    //         println!(
+    //             "[kernel] Data Abort @ {:#x}, FAR = {:#x}, ISS = {:#x}, kernel killed it.",
+    //             tf.elr,
+    //             FAR_EL1.get(),
+    //             iss
+    //         );
+    //         CurrentTask::get().exit(-1);
+    //     }
+    //     Some(ESR_EL1::EC::Value::InstrAbortLowerEL)
+    //     | Some(ESR_EL1::EC::Value::InstrAbortCurrentEL) => {
+    //         let iss = esr.read(ESR_EL1::ISS);
+    //         println!(
+    //             "[kernel] Instruction Abort @ {:#x}, FAR = {:#x}, ISS = {:#x}, kernel killed it.",
+    //             tf.elr,
+    //             FAR_EL1.get(),
+    //             iss
+    //         );
+    //         CurrentTask::get().exit(-1);
+    //     }
+    //     _ => {
+    //         panic!(
+    //             "Unsupported synchronous exception @ {:#x}: ESR = {:#x} (EC {:#08b}, ISS {:#x})",
+    //             tf.elr,
+    //             esr.get(),
+    //             esr.read(ESR_EL1::EC),
+    //             esr.read(ESR_EL1::ISS),
+    //         );
+    //     }
+    // }
+}
+
+#[no_mangle]
+fn handle_irq_exception(_tf: &mut TrapFrame) {
+    // if crate::gicv2::handle_irq() == IrqHandlerResult::Reschedule {
+    //     CurrentTask::get().yield_now();
+    // }
+}
